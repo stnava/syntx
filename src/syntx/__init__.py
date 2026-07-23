4f75f1a3451817480a19c39006d95d0add2ab026
stnava <stnava@gmail.com>
2026-07-23 17:20:15 -0400

    release: bump version to v0.1.22

--- src/syntx/__init__.py
+++ src/syntx/__init__.py
@@ -8,7 +8,7 @@ from .generators import CrossProductGenerator
 # Expose both syn and registration to satisfy user requirements
 syn = registration
 
-__version__ = "0.1.21"
+__version__ = "0.1.22"
 
 
 __all__ = [
