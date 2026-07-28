86fdf26f5fb8eb781b0495576fe54910e0d3ce4c
stnava <stnava@gmail.com>
2026-07-28 14:41:24 -0400

    release: bump version to v1.0.5

--- src/syntx/__init__.py
+++ src/syntx/__init__.py
@@ -1,20 +1,37 @@
-from .syn import registration, SyNTo, calculate_inverse_identity_error, auto_reg
+from .syn import (
+    registration,
+    SyNTo,
+    calculate_inverse_identity_error,
+    auto_reg,
+    normalize_tensor,
+    plot_deformation_grid,
+    plot_edge_overlay,
+    render_standard_4panel
+)
 from .syn_jax import SyNTo as SyNToJax
 from .transform import SyNToTransform
 from .features import FeatureSpaceLoss, VGG19Extractor, DINOv2Extractor, ResNet10Extractor, SwinUNETRExtractor
 from .image_compare import image_compare
 from .generators import CrossProductGenerator
+from .tvf import TVFModel
+from .tvf_jax import TVFModelJAX
+from .reporting import create_registration_report
 
 # Expose syn, registration, and auto_reg
 syn = registration
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 __all__ = [
     "syn",
     "registration",
     "auto_reg",
+    "normalize_tensor",
+    "plot_deformation_grid",
+    "plot_edge_overlay",
+    "render_standard_4panel",
+    "create_registration_report",
     "SyNTo",
     "SyNToJax",
     "SyNToTransform",
@@ -26,4 +43,8 @@ __all__ = [
     "SwinUNETRExtractor",
     "image_compare",
     "CrossProductGenerator",
+    "TVFModel",
+    "TVFModelJAX",
 ]
+
+
